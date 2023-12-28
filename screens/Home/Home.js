45813d8013bf3fbd02648274eb3ef6895b242f43
Burcu <macbookair@MacBook-MacBook-Air.local>
2023-12-28 21:17:23 +0300

    created button component

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -3,10 +3,18 @@ import style from './style';
 import {SafeAreaView, Text, View} from 'react-native';
 import globalStyle from '../../assets/styles/globalStyle';
 import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <Header title={'John'} type={1} />
+      <Button
+        title={'Donate'}
+        onPress={() => {
+          console.log('You just pressed me!');
+        }}
+      />
+      <Button title={'Donate'} isDisabled={true} />
     </SafeAreaView>
   );
 };
