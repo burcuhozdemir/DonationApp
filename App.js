8eb86cce39b7da2e488ff95e1f63ffd06ffdc005
Burcu <macbookair@MacBook-MacBook-Air.local>
2023-12-28 20:51:26 +0300

    navigation setup, scaling functions & device info installation

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import {getFontFamily} from './assets/fonts/helper';
+import MainNavigation from './navigation/MainNavigation';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text style={{fontSize: 50, fontFamily: getFontFamily('Inter', '100')}}>
-        Hello World!
-      </Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <MainNavigation />
+    </NavigationContainer>
   );
 };
 
