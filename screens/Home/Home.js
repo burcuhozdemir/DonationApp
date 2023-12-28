fb9e1835d9fe5dcbcb3284f12c9b40a6b45797b5
Burcu <macbookair@MacBook-MacBook-Air.local>
2023-12-28 21:27:09 +0300

    created tab and badge component

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -4,17 +4,15 @@ import {SafeAreaView, Text, View} from 'react-native';
 import globalStyle from '../../assets/styles/globalStyle';
 import Header from '../../components/Header/Header';
 import Button from '../../components/Button/Button';
+import Tab from '../../components/Tab/Tab';
+import Badge from '../../components/Badge/Badge';
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <Header title={'John'} type={1} />
-      <Button
-        title={'Donate'}
-        onPress={() => {
-          console.log('You just pressed me!');
-        }}
-      />
-      <Button title={'Donate'} isDisabled={true} />
+      <Tab title={'Highlight'} />
+      <Tab title={'Highlight'} isInactive={true} />
+      <Badge title={'Environment'} />
     </SafeAreaView>
   );
 };
