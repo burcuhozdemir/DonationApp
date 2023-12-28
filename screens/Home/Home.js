ffa90b9281225786832d7f5b26e576a905eadbb7
Burcu <macbookair@MacBook-MacBook-Air.local>
2023-12-28 21:15:00 +0300

    created header component

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -2,13 +2,11 @@ import React from 'react';
 import style from './style';
 import {SafeAreaView, Text, View} from 'react-native';
 import globalStyle from '../../assets/styles/globalStyle';
-
+import Header from '../../components/Header/Header';
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      <View>
-        <Text>Hello</Text>
-      </View>
+      <Header title={'John'} type={1} />
     </SafeAreaView>
   );
 };
