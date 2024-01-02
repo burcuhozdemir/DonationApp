c5f743bafaf6d2e10a32694a7e4cab5d45b69a1e
Burcu <macbookair@MacBook-MacBook-Air.local>
2024-01-02 23:52:15 +0300

    used user reducer for building home header

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import style from './style';
-import {Pressable, SafeAreaView, Text, View} from 'react-native';
+import {
+  Pressable,
+  SafeAreaView,
+  Text,
+  View,
+  ScrollView,
+  Image,
+} from 'react-native';
 import globalStyle from '../../assets/styles/globalStyle';
 import Header from '../../components/Header/Header';
-import Button from '../../components/Button/Button';
-import Tab from '../../components/Tab/Tab';
-import Badge from '../../components/Badge/Badge';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faSearch} from '@fortawesome/free-solid-svg-icons';
-import Search from '../../components/Search/Search';
-import SingleDonationItem from '../../components/SingleDonationItem/SingleDonationItem';
 import {useDispatch, useSelector} from 'react-redux';
-import {updateFirstName} from '../../redux/reducers/User';
+import {resetToInitialState, updateFirstName} from '../../redux/reducers/User';
+import Search from '../../components/Search/Search';
+
 const Home = () => {
   const user = useSelector(state => state.user);
   const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      <Header title={user.firstName + ' ' + user.lastName} type={1} />
-      <Pressable onPress={() => dispatch(updateFirstName({firstName: 'Tomy'}))}>
-        <Text>Press me to change first name</Text>
-      </Pressable>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={style.header}>
+          <View>
+            <Text style={style.headerIntroText}>Hello,</Text>
+            <View style={style.username}>
+              <Header title={user.firstName + ' ' + user.lastName} type={1} />
+            </View>
+          </View>
+          <Image
+            source={{uri: user.profileImage}}
+            style={style.profileImage}
+            resizeMode={'contain'}
+          />
+        </View>
+        <View style={style.searchBox}>
+          <Search />
+        </View>
+        <Pressable style={style.highlightedImageContainer}>
+          <Image
+            style={style.highlightedImage}
+            source={require('../../assets/images/highlighted_image.png')}
+            resizeMode={'contain'}
+          />
+        </Pressable>
+      </ScrollView>
     </SafeAreaView>
   );
 };
